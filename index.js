a3b64682540abab4261842cd5892273a89a1dae5
thetripleacoder <85655422+thetripleacoder@users.noreply.github.com>
2023-11-22 01:11:00 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var cors = require('cors');
+require('dotenv').config();
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
+
+var app = express();
+
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function (req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
+});
+
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {
+  // req.file is the name of your file in the form above, here 'uploaded_file'
+  // req.body will hold the text fields, if there were any
+  console.log(req.file, req.body);
+  res.send({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size,
+  });
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log('Your app is listening on port ' + port);
+});
